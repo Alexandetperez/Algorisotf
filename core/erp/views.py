6051656b15c621e7a95a513fe4ff6a535cbd74f8
Alexandetperez <baperez991@misena.edu.co>
2023-07-12 15:10:21 -0500

    Video 17

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from core.erp.models import Category,Product
 
 
 def myfirstview(request):
-    data = {"name": "Alexander"}
+    data = {"name": "Alexander",
+            'category': Category.objects.all()
+             }
+    
     return render(request, "index.html", data)
+
+
+def mysecondview(request):
+    data = {"name": "Alexander",
+            'productos': Product.objects.all()
+             }
+    
+    return render(request, "second.html", data)
